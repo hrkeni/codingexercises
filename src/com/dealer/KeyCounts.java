aa1413758d21b7d4a47e93823c7c0fa025e55393
Harshith Keni <harshith.keni@sdstate.edu>
2016-05-16 22:57:41 -0500

    basic skeletal project structure

---
+++ src/com/dealer/KeyCounts.java
@@ -0,0 +1,7 @@
+package com.dealer;
+
+/**
+ * Created by harshith on 5/16/16.
+ */
+public class KeyCounts {
+}
---
+++ test/com.dealer/KeyCountsTest.java
@@ -0,0 +1,14 @@
+package com.dealer;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by harshith on 5/16/16.
+ */
+public class KeyCountsTest extends TestCase{
+    @Test
+    public void testKeyCounts() {
+        fail("Oops");
+    }
+}
